022e49e885ac22d505ea78d573ccdf53fd32484d
Saul Echezuria <saulechezuriarojas@gmail.com>
2022-07-14 21:46:06 -0400

    commit inicial poema bucle

---
+++ js/script.js
@@ -0,0 +1,15 @@
+for(let i=11; i>0;i--){
+    
+}
+
+if (i= 11) { alert("La vida inicia cuando empezamos a morir")}
+if (i= 10) { alert("Cuando el reloj de nuestro tiempo echa sus arenas a andar");
+if (i= 9) { alert("Asi la vida es el inicio del fin")}}
+if (i= 8) { alert("Y cuando olvidamos esto andamos como muertos en lugar de vivir")}
+if (i= 7) { alert("Por eso la vida nos recuerda constantemente la muerte")}
+if (i= 6) { alert("Perdiendo a quienes amamos logramos despertar")}
+if (i= 5) { alert("Recordamos que el tiempo es precisoso, invaluable e irremplazable")}
+if (i= 4) { alert("Que cada dia que pasa, es un dia menos que tenemos")}
+if (i= 3) { alert("Que como este poema finito que esta a punto de terminar")}
+if (i= 2) { alert("Debemos valorar aquello que tiene verdad y no aquello que olvidamos es transitorio")}
+if (i= 1) { alert("Fin. Saul Echezuria, Santiago de Chile Julio 2022")}
