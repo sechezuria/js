1cb19d049e18617cf75bbcd663fe6349a8ce2eac
Saul Echezuria <saulechezuriarojas@gmail.com>
2022-07-19 12:46:34 -0400

    carga primer desafio

--- js/script.js
+++ js/script.js
@@ -1,15 +1,63 @@
-for(let i=11; i>0;i--){
-    
+// Tienes 3 intentos para constreseña
+// 
+// Si la coloca bien = Saluda y pide nuevos datos
+// Ingresar meses de antiguedad de la empresa
+// 1.5 dias por mes de trabajo
+
+// Ingresar dias de vacaciones ya tomados
+// Si no se ha tomado dias de vacaciones - hace el calculo total
+// Si tiene, se hace el calculo restando los dias tomados
+
+
+let savedPass = "1"
+
+
+for (let i = 0; i <= 2; i++) {
+    let password = prompt("ingresa tu contraseña");
+    console.log(i, password, i === 2)
+    if (i === 2 && savedPass !== password) {
+        alert("Ha ingresado incorrectamente su contraseña demasiadas veces");
+    }
+    if (savedPass === password) {
+        alert("Contraseña Ingresada Correctamente");
+        saludarConNombre();
+        break;
+    } else if (i < 2 && savedPass !== password) {
+        alert("Contraseña invalida");
+        continue;
+    }
 }
 
-if (i= 11) { alert("La vida inicia cuando empezamos a morir")}
-if (i= 10) { alert("Cuando el reloj de nuestro tiempo echa sus arenas a andar");
-if (i= 9) { alert("Asi la vida es el inicio del fin")}}
-if (i= 8) { alert("Y cuando olvidamos esto andamos como muertos en lugar de vivir")}
-if (i= 7) { alert("Por eso la vida nos recuerda constantemente la muerte")}
-if (i= 6) { alert("Perdiendo a quienes amamos logramos despertar")}
-if (i= 5) { alert("Recordamos que el tiempo es precisoso, invaluable e irremplazable")}
-if (i= 4) { alert("Que cada dia que pasa, es un dia menos que tenemos")}
-if (i= 3) { alert("Que como este poema finito que esta a punto de terminar")}
-if (i= 2) { alert("Debemos valorar aquello que tiene verdad y no aquello que olvidamos es transitorio")}
-if (i= 1) { alert("Fin. Saul Echezuria, Santiago de Chile Julio 2022")}
+function saludarConNombre () {
+    let nombreUsuario = prompt("Ingresa tu nombre");
+    alert("Bienvenido" + " " + nombreUsuario);
+    if (nombreUsuario) {
+        ingresarDatos();
+    }
+}
+
+function ingresarDatos () {
+    let antiguedad = prompt("Ingresa número de meses en la empresa");
+    let i = parseInt(antiguedad);
+    let anosLaborales = i / 12;
+    var conDecimal = anosLaborales.toFixed(1);
+    alert('LLevas' + " " + conDecimal + " " + "años de antiguedad laboral");
+    let diasTomados = prompt("Ingresa dias de vacaciones ya tomados");
+    let resultado = (antiguedad * 1.5) - diasTomados;
+    alert("Tienes disponibles" + " "  + resultado + " " + "días de vacaciones");
+    alert("Recuerda que cada mes acumulas 1.5 dias de vacaciones, en caso de hacer otra consulta presiona F5");
+   
+   
+   
+   
+/*    Revisiones futuras para validar datos */
+    // if (isNaN(resultado)) {
+    //     alert("no ingreso un numero");
+    //     ingresarDatos()
+    // }
+    // if (!isNaN(resultado)) {
+    //     alert("Tienes disponibles" + " " + resultado + " " + "días de vacaciones");
+   // }
+}
+
+
