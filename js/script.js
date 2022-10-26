bfc971c177e795124d6fe45c83952520b852b533
Saul Echezuria <saulechezuriarojas@gmail.com>
2022-10-26 13:44:22 -0300

    eventos y locals

--- js/script.js
+++ js/script.js
@@ -27,9 +27,8 @@ const users = [{
     monthsInCharge: 36,
     daysOfVacationsUsed: 10
 }
+
 ]
-
-
 
 let userName;
 let password;
@@ -37,7 +36,20 @@ let antiguedad;
 let diasTomados;
 let i = parseInt(antiguedad);
 
+const nombre = document.getElementById('user');
 
+nombre.addEventListener('input', (event) => {
+    if (nombre.value.length > 0) {
+        document.getElementById("boton").textContent = "Ingresar";
+    }
+    else {
+        document.getElementById("boton").textContent = "Registro";
+    }
+}, true);
+
+function pulsaciones() {
+    window.alert("El numero total de pulsaciones es: " + users)
+}
 const submit = () => {
     userName = document.getElementById('user').value
     password = document.getElementById('password').value
@@ -48,12 +60,17 @@ const submit = () => {
     for (let i = 0; i <= users.length; i++) {
         if (userName === users[i].name) {
             if (password === users[i].password) {
+                var retrievedObject = localStorage.getItem('testObject');
+                window.alert(retrievedObject['lastlogin']);
+                users[i].lastlogin = Date.now();
                 antiguedad = users[i].monthsInCharge
                 diasTomados = users[i].daysOfVacationsUsed
                 let resultado = (antiguedad * 1.25) - diasTomados;
                 form.style.display = 'none';
                 loggedIn.style.display = 'flex';
                 vacationLeft.innerHTML = "Tienes disponibles" + " " + resultado + " " + "días de vacaciones"
+                var testObject = { 'nombre': userName, 'lastlogin': Date.now()};
+                localStorage.setItem('testObject', testObject);
                 break
             } else {
                 alert('La contrasena es incorrecta')
